20662c65aee7b89b4d5cb2f989dd19117ff00b03
Trenton Arthur <tarthur@chicagobooth.edu>
2014-11-06 14:27:06 -0600

    Completed baseline assignment
    Updated page to have full CRUD functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,45 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @index = params["id"]
+    @chosenPhoto = Photo.find_by("id" => @index )
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    @new_photo = Photo.new
+    @new_photo.caption = params["the_caption"]
+    @new_photo.source = params["the_source"]
+    @new_photo.save
+
+  end
+
+  def destroy
+
+    @index = params["id"]
+    @destroyPhoto = Photo.find_by("id" => @index )
+    @destroyPhoto.destroy
+
+  end
+
+  def edit_form
+    @index = params["id"]
+    @editPhoto = Photo.find_by("id" => @index )
+  end
+
+  def update_row
+    @index = params["id"]
+    @photoEdits = Photo.find_by("id" => @index )
+    @photoEdits.caption = params["the_caption"]
+    @photoEdits.source = params["the_source"]
+    @photoEdits.save
+
+
+  end
 
 end
